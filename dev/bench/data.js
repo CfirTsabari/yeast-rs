3e0d7df9cf327fcd2fd783d88051fb73091bc9ab
github-action-benchmark <github@users.noreply.github.com>
2021-07-20 21:40:28 +0000

    add Rust Benchmark (cargo) benchmark result for 50ae85ae172784f2f91c16e64ec065895e2f5ac4

---
+++ dev/bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1626817228895,
+  "repoUrl": "https://github.com/CfirTsabari/yeast-rs",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "cfir16@gmail.com",
+            "name": "CfirTsabari",
+            "username": "CfirTsabari"
+          },
+          "committer": {
+            "email": "cfir16@gmail.com",
+            "name": "CfirTsabari",
+            "username": "CfirTsabari"
+          },
+          "distinct": true,
+          "id": "50ae85ae172784f2f91c16e64ec065895e2f5ac4",
+          "message": "ci: changed output of benchmark",
+          "timestamp": "2021-07-21T00:26:36+03:00",
+          "tree_id": "c597891392fa9cab40458bfb075db0371f030640",
+          "url": "https://github.com/CfirTsabari/yeast-rs/commit/50ae85ae172784f2f91c16e64ec065895e2f5ac4"
+        },
+        "date": 1626817228396,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "async_std_yeast",
+            "value": 434,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yeast",
+            "value": 431,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tokio_yeast",
+            "value": 483,
+            "range": "± 9",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
